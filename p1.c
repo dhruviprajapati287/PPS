316a65d802b7be03b69c93a4d39b809a9f1e677a
dhruviprajapati287 <dhruviprajapatid99@gmail.com>
2026-02-17 08:31:50 -0800

    Create p1.c

---
+++ p1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  float a,b;
+  printf("\n enter value of a and b");
+  scanf("%f %f" ,&a,&b);
+  printf("\n sum of int =%f",a+b);
+  printf("\n subtraction of int=%f",a-b);
+  printf("\n multiplication of int=%f", a*b);
+  printf(" \n divide of int=%f", a/b);
+  printf("\n modulo is:%d", (int)a%(int)b);
+  return 0;
+}
+
+
+   
